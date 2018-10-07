94380c06739287870ca52f2f82b1eeaf6acd8f16
Solon Aguiar <solon.liam.aguiar@gmail.com>
2018-10-07 11:56:57 -0400

    Create Unit module for conversions

---
+++ lib/mission_control/unit.rb
@@ -0,0 +1,17 @@
+module Unit
+  def self.ms_to_se(ms)
+    ms.to_f/1000
+  end
+
+  def self.se_to_ms(se)
+    se.to_f * 1000
+  end
+
+  def self.km_per_hour_to_km_per_se(km_per_hour)
+    km_per_hour.to_f / 3600
+  end
+
+  def self.liters_per_minute_to_liters_per_sec(liters_per_minute)
+    liters_per_minute.to_f / 60
+  end
+end
