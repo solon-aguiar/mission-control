be249477172783296b4dccf6b2d81d5ff13ad728
Solon Aguiar <solon.liam.aguiar@gmail.com>
2018-10-07 10:32:07 -0400

    Add space after posing a question

--- lib/mission_control/io/input.rb
+++ lib/mission_control/io/input.rb
@@ -3,7 +3,7 @@ require_relative './output'
 module IO::Input
   def self.get_option(message, options, invalid_prompt)
     loop do
-      IO::Output::write(message)
+      IO::Output::write("#{message} ")
       option = $stdin.gets.chomp.strip
       return options[option] if options.has_key? option
       IO::Output::write_line(invalid_prompt)
@@ -12,9 +12,9 @@ module IO::Input
 
   def self.get_mission_name(message, invalid_prompt)
     loop do
-      IO::Output::write(message)
+      IO::Output::write("#{message} ")
       response = $stdin.gets.chomp
-      return response unless response.empty?
+      return response unless response.strip.empty?
       IO::Output::write_line(invalid_prompt)
     end
   end
--- spec/mission_control/io/input_spec.rb
+++ spec/mission_control/io/input_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe IO::Input do
   describe 'get_option' do
     it 'maps valid input to appropriate symbol' do
       allow($stdin).to receive(:gets).once.and_return('blue')
-      allow(IO::Output).to receive(:write).once.with(choice_message)
+      allow(IO::Output).to receive(:write).once.with("#{choice_message} ")
 
       option = described_class::get_option(choice_message, options, invalid_prompt)
       expect(option).to be(:blue)
@@ -21,7 +21,7 @@ RSpec.describe IO::Input do
 
     it 'does not accept invalid options' do
       allow($stdin).to receive(:gets).exactly(3).times.and_return('bl', "\n", 'blue')
-      allow(IO::Output).to receive(:write).exactly(3).times.with(choice_message)
+      allow(IO::Output).to receive(:write).exactly(3).times.with("#{choice_message} ")
       allow(IO::Output).to receive(:write_line).exactly(2).times.with(invalid_prompt)
 
       option = described_class::get_option(choice_message, options, invalid_prompt)
@@ -30,7 +30,7 @@ RSpec.describe IO::Input do
 
     it 'ignores whitespaces' do
       allow($stdin).to receive(:gets).once.and_return(' red ')
-      allow(IO::Output).to receive(:write).once.with(choice_message)
+      allow(IO::Output).to receive(:write).once.with("#{choice_message} ")
 
       option = described_class::get_option(choice_message, options, invalid_prompt)
       expect(option).to be(:red)
@@ -38,7 +38,7 @@ RSpec.describe IO::Input do
 
     it 'ignores newlines' do
       allow($stdin).to receive(:gets).once.and_return(" red \n")
-      allow(IO::Output).to receive(:write).once.with(choice_message)
+      allow(IO::Output).to receive(:write).once.with("#{choice_message} ")
 
       option = described_class::get_option(choice_message, options, invalid_prompt)
       expect(option).to be(:red)
@@ -48,25 +48,25 @@ RSpec.describe IO::Input do
   describe 'get_string' do
     it 'accepts a non-empty input' do
       allow($stdin).to receive(:gets).once.and_return(name)
-      allow(IO::Output).to receive(:write).once.with(question)
+      allow(IO::Output).to receive(:write).once.with("#{question} ")
 
       response = described_class::get_mission_name(question, invalid_prompt)
       expect(response).to eq(name)
     end
 
     it 'does not accept invalid inputs' do
-      allow($stdin).to receive(:gets).exactly(3).times.and_return('', "\n", name)
-      allow(IO::Output).to receive(:write).exactly(3).times.with(question)
-      allow(IO::Output).to receive(:write_line).exactly(2).times.with(invalid_prompt)
+      allow($stdin).to receive(:gets).exactly(4).times.and_return('', '  ', "\n", name)
+      allow(IO::Output).to receive(:write).exactly(4).times.with("#{question} ")
+      allow(IO::Output).to receive(:write_line).exactly(3).times.with(invalid_prompt)
 
       response = described_class::get_mission_name(question, invalid_prompt)
       expect(response).to eq(name)
     end
 
-    it 'does not ignore spaces' do
+    it 'does not ignore spaces for valid inputs' do
       padded_name = "  #{name} "
       allow($stdin).to receive(:gets).once.and_return(padded_name)
-      allow(IO::Output).to receive(:write).once.with(question)
+      allow(IO::Output).to receive(:write).once.with("#{question} ")
 
       response = described_class::get_mission_name(question, invalid_prompt)
       expect(response).to eq(padded_name)
